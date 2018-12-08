ec5ee914b2cf0b5fac0792cc1e75eec77904d6cc
sidharth <sidharth@knoldus.com>
2018-12-08 18:49:16 +0530

    add example 7

--- src/main/scala/com/knoldus/functionalconf2018/examples/Application.scala
+++ src/main/scala/com/knoldus/functionalconf2018/examples/Application.scala
@@ -1,5 +1,43 @@
 package com.knoldus.functionalconf2018.examples
 
+import cats.effect.IO
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object Application {
 
+    // Memoization
+    def addToGaugeIO(i: Int): IO[Int] = IO {
+        println("Execute addToGaugeIO ... ")
+        i
+    }
+
+    println("IO addToGaugeIO First")
+    val result1 = for {
+        a <- addToGaugeIO(13)
+        b <- addToGaugeIO(13)
+    } yield a + b
+
+    println(result1.unsafeRunSync())
+
+    println("IO addToGaugeIO Second")
+    val x = addToGaugeIO(13)
+    val result2 = for {
+        a <- x
+        b <- x
+    } yield a + b
+
+    println(result2.unsafeRunSync())
+
+    println("Future addToGaugeFuture")
+    def addToGaugeFuture(i : Int): Future[Int] = Future { i }
+    val y = addToGaugeFuture(13)
+    val result3 = for {
+        a <- y
+        b <- y
+    } yield a + b
+
+    result3.foreach(println)
+
+    Thread.sleep(1000)
 }
