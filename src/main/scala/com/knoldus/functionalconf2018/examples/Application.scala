a4d78eec5a0ca6848f64230061813555c92c02f3
sidharth <sidharth@knoldus.com>
2018-12-08 18:53:37 +0530

    add example 9

--- src/main/scala/com/knoldus/functionalconf2018/examples/Application.scala
+++ src/main/scala/com/knoldus/functionalconf2018/examples/Application.scala
@@ -1,5 +1,15 @@
 package com.knoldus.functionalconf2018.examples
 
-object Application {
+import pureconfig.error.ConfigReaderFailures
+import pureconfig.generic.auto._
 
+object Application extends App {
+
+    sealed trait Database
+    final case class Settings(driver: String, url: String, user: String, password: String) extends Database
+
+    case class ApplicationConf(database: Database)
+
+    val databaseConfig : Either[ConfigReaderFailures, ApplicationConf] = pureconfig.loadConfig[ApplicationConf]
+    databaseConfig.foreach(println)
 }
