f06f9ca928844e964738622bc5426173a5c8a20e
sidharth <sidharth@knoldus.com>
2018-12-08 19:02:40 +0530

    add unit test case for example 1

--- src/test/scala/com/knoldus/functionalconf2018/examples/ApplicationSpec.scala
+++ src/test/scala/com/knoldus/functionalconf2018/examples/ApplicationSpec.scala
@@ -1,6 +1,17 @@
 package com.knoldus.functionalconf2018.examples
 
-class ApplicationSpec
-{
+import cats.effect.IO
+import org.http4s.{Method, Request, Status, Uri}
+import org.scalatest.{Matchers, WordSpecLike}
 
+class ApplicationSpec extends WordSpecLike with Matchers {
+
+    "Request" should {
+        "should return greeting with Hello " in {
+            val mockRequest = Request[IO](method = Method.GET, uri = Uri.uri("/hello/Singh") )
+            val response = new Application().httpRoutes.run(mockRequest).value.unsafeRunSync()
+            response.get.status should === (Status.Ok)
+            response.get.as[String].unsafeRunSync() should === ("Hello Singh")
+        }
+    }
 }
