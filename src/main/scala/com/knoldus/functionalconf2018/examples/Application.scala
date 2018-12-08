1b89b0715545d5589c61b796f2117de3bd05f4c3
sidharth <sidharth@knoldus.com>
2018-12-08 18:48:00 +0530

    add example 6

--- src/main/scala/com/knoldus/functionalconf2018/examples/Application.scala
+++ src/main/scala/com/knoldus/functionalconf2018/examples/Application.scala
@@ -1,5 +1,26 @@
 package com.knoldus.functionalconf2018.examples
 
-object Application {
 
+import cats.effect.IO
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.Random
+
+object Application extends App {
+
+    // Execute Eagerly
+    val hello = Future { println("Hello") }
+    val world = Future { println("World") }
+
+    //    hello.flatMap(_ => world)
+
+    // Referential transparent
+    for {
+        _ <- Future { println(s"Future Execution: ${Random.nextInt(13)} ") }
+    } yield ()
+
+
+    val ioResult = for {
+        _ <- IO { println(s"IO Execution: ${Random.nextInt(13)} ") }
+    } yield ()
 }
